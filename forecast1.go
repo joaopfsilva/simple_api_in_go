6fd77709e91dab950cee3799a29e28b83b2d0abe
joaopfsilva <j0a0.silva.kle@gmail.com>
2020-03-01 13:07:44 +0100

    insert into db, handling duplicated supplier(name) done.

--- forecast1.go
+++ forecast1.go
@@ -15,6 +15,8 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"os/exec"
+	"strconv"
 
 	_ "github.com/go-sql-driver/mysql"
 )
@@ -37,7 +39,7 @@ type Suppliers struct {
 type Supplier struct {
 	XMLName xml.Name `xml:"supplier"`
 	Name    string   `xml:"name,attr"`
-	Age     int8     `xml:"age,attr"`
+	Age     string   `xml:"age,attr"`
 }
 
 func readXML(filename string, db *sql.DB) Suppliers {
@@ -76,6 +78,7 @@ func checkCount(rows *sql.Rows) (count int) {
 }
 
 func DBstats() {
+	fmt.Println("###")
 	res, _ := db.Query("SELECT COUNT(*) FROM suppliers;")
 	totalSuppliers := checkCount(res)
 	fmt.Printf("Total Suppliers: %d\n\n", totalSuppliers)
@@ -84,11 +87,13 @@ func DBstats() {
 func insertIntoDB(suppliers Suppliers) {
 	var errors int = 0
 	for i := 0; i < len(suppliers.Supplier); i++ {
-		_, err := db.Exec("INSERT INTO suppliers (name, age) VALUES (?, ?)", suppliers.Supplier[i].Name, suppliers.Supplier[i].Age)
-		if err != nil {
-			errors++
-		}
+		age, _ := strconv.Atoi(suppliers.Supplier[i].Age)
+		name := suppliers.Supplier[i].Name
+
+		_, err := db.Exec(fmt.Sprintf("INSERT INTO suppliers (name, age) VALUES ('%s', %d) ON DUPLICATE KEY UPDATE age = %d;", name, age, age))
+		handleError(err)
 	}
+
 	fmt.Printf("Successfully inserted values %d of %d\n", len(suppliers.Supplier)-errors, len(suppliers.Supplier))
 }
 
@@ -101,32 +106,63 @@ func handleError(err error) {
 func configDB() {
 	var err error
 	// create MySQL database.
-	dbinfo := fmt.Sprintf("%s@/", dbUser)
+	dbinfo := fmt.Sprintf("%s@/%s", dbUser, dbName)
 	db, err = sql.Open("mysql", dbinfo)
 
 	_, err = db.Exec(fmt.Sprintf("CREATE DATABASE IF NOT EXISTS %s", dbName))
 	handleError(err)
 
+	// select created database
 	_, err = db.Exec(fmt.Sprintf("USE %s", dbName))
 	handleError(err)
 
+	// create table Suppliers(id, name, age, created_at, updated_at)
 	_, err = db.Exec("CREATE TABLE IF NOT EXISTS suppliers (id INTEGER PRIMARY KEY AUTO_INCREMENT, name varchar(255) NOT NULL UNIQUE, age INTEGER NOT NULL, created_at datetime DEFAULT NULL DEFAULT NOW(), updated_at datetime DEFAULT NULL DEFAULT NOW() ON UPDATE NOW())")
 	handleError(err)
 
-	// just a check
-	_, err = db.Exec("SELECT COUNT(*) FROM suppliers;")
+	// just a sanity check
+	_, err = db.Query("SELECT COUNT(*) FROM suppliers;")
 	handleError(err)
 }
 
-func showMenu() {
-	var input string
+// DBListSuppliers list all suppliers in database
+func DBListSuppliers() {
+	_, err := db.Query("SELECT name, age FROM suppliers;")
+	handleError(err)
 
+	// for res.Next() {
+	// 	var name string
+	// 	// var age int8
+	// 	fmt.Println(res.Scan(&name))
+	// }
+}
+
+func showMenu() {
 	fmt.Println("1- List suppliers")
 	fmt.Println("2- Age of supplier")
 	fmt.Print("Enter option: ")
+}
 
+func readUserOption() int8 {
+	var input int8
 	fmt.Scanln(&input)
-	fmt.Print(input)
+	return input
+}
+
+func processMenuOption(option int8) {
+	// clear screen
+	c := exec.Command("clear")
+	c.Stdout = os.Stdout
+	c.Run()
+	switch option {
+	case 1:
+		fmt.Println("List Suppliers")
+		DBListSuppliers()
+	case 2:
+		fmt.Println("two")
+	default:
+		fmt.Println("default")
+	}
 }
 
 func main() {
@@ -135,10 +171,17 @@ func main() {
 		fmt.Println("Missing XML file")
 	} else {
 		configDB()
+
 		xmlData := readXML(os.Args[1], db)
+
 		insertIntoDB(xmlData)
-		DBstats()
-		showMenu()
+		// DBstats()
+
+		// DBListSuppliers()
+		// showMenu()
+		// option := readUserOption()
+		// processMenuOption(option)
+
 	}
 
 	defer db.Close()
