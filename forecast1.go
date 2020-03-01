0fc06883771959cb5deaf8c00076d8ba4c3fffa1
joaopfsilva <j0a0.silva.kle@gmail.com>
2020-03-01 10:55:12 +0100

    configured database. inserting values done. unique and validations missing

--- forecast1.go
+++ forecast1.go
@@ -3,6 +3,10 @@
 // 	<supplier name="Redstick" age="20"/>
 // </suppliers>
 
+// NOTES
+// assign -> =
+// initialise a new variable and assign -> :=
+
 package main
 
 import (
@@ -11,9 +15,16 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
-	"strconv"
 
 	_ "github.com/go-sql-driver/mysql"
+)
+
+var db *sql.DB
+
+const (
+	dbUser     = "root"
+	dbPassword = ""
+	dbName     = "webshopdb"
 )
 
 // Suppliers xml structure
@@ -26,19 +37,18 @@ type Suppliers struct {
 type Supplier struct {
 	XMLName xml.Name `xml:"supplier"`
 	Name    string   `xml:"name,attr"`
-	Age     string   `xml:"age,attr"`
+	Age     int8     `xml:"age,attr"`
 }
 
-func processXML(filename string, db *sql.DB) string {
+func readXML(filename string, db *sql.DB) Suppliers {
 	// Open our xmlFile
 	xmlFile, err := os.Open(filename)
 	// if we os.Open returns an error then handle it
-	if err != nil {
-		fmt.Println(err)
-	}
+	handleError(err)
 
 	// read our opened xmlFile as a byte array.
-	byteValue, _ := ioutil.ReadAll(xmlFile)
+	byteValue, err := ioutil.ReadAll(xmlFile)
+	handleError(err)
 
 	// we initialize our Supplier array
 	var suppliers Suppliers
@@ -47,35 +57,88 @@ func processXML(filename string, db *sql.DB) string {
 	xml.Unmarshal(byteValue, &suppliers)
 
 	// output XML file content
-	for i := 0; i < len(suppliers.Supplier); i++ {
-		name := suppliers.Supplier[i].Name
-		age, _ := strconv.Atoi(suppliers.Supplier[i].Age)
-		row, err := db.Query(fmt.Sprintf("INSERT INTO suppliers (name, age) VALUES ('%s', %d)", name, age))
-		fmt.Println(err)
-		fmt.Println(row)
-	}
+	// for i := 0; i < len(suppliers.Supplier); i++ {
+	// 	name := suppliers.Supplier[i].Name
+	// 	age := suppliers.Supplier[i].Age
+	// 	// row, err := db.Query(fmt.Sprintf("INSERT INTO suppliers (name, age) VALUES ('%s', %d)", name, age))
+	// 	fmt.Println(name)
+	// 	fmt.Println(age)
+	// }
 
 	// defer the closing of our xmlFile so that we can parse it later on
 	defer xmlFile.Close()
-	return fmt.Sprintf("Successfully Opened %s", filename)
+	return suppliers
 }
 
-func main() {
-	// create MySQL database.
-	db, err := sql.Open("mysql", "root@/")
-	db.Exec("CREATE DATABASE IF NOT EXISTS webshopdb")
-	db.Exec("USE webshopdb")
+func checkCount(rows *sql.Rows) (count int) {
+	rows.Scan(&count)
+	return count
+}
+
+func DBstats() {
+	res, _ := db.Query("SELECT COUNT(*) FROM suppliers;")
+	totalSuppliers := checkCount(res)
+	fmt.Printf("Total Suppliers: %d\n\n", totalSuppliers)
+}
+
+func insertIntoDB(suppliers Suppliers) {
+	var errors int = 0
+	for i := 0; i < len(suppliers.Supplier); i++ {
+		_, err := db.Exec("INSERT INTO suppliers (name, age) VALUES (?, ?)", suppliers.Supplier[i].Name, suppliers.Supplier[i].Age)
+		if err != nil {
+			errors++
+		}
+	}
+	fmt.Printf("Successfully inserted values %d of %d\n", len(suppliers.Supplier)-errors, len(suppliers.Supplier))
+}
+
+func handleError(err error) {
 	if err != nil {
 		panic(err.Error())
 	}
+}
 
-	db.Exec("CREATE TABLE IF NOT EXISTS suppliers (id INTEGER PRIMARY KEY AUTO INCREMENR, name TEXT, age INTEGER)")
-	db.Exec("SELECT COUNT(*) FROM suppliers;")
+func configDB() {
+	var err error
+	// create MySQL database.
+	dbinfo := fmt.Sprintf("%s@/", dbUser)
+	db, err = sql.Open("mysql", dbinfo)
 
-	if len(os.Args) == 1 {
-		fmt.Println("No argument to process")
+	_, err = db.Exec(fmt.Sprintf("CREATE DATABASE IF NOT EXISTS %s", dbName))
+	handleError(err)
+
+	_, err = db.Exec(fmt.Sprintf("USE %s", dbName))
+	handleError(err)
+
+	_, err = db.Exec("CREATE TABLE IF NOT EXISTS suppliers (id INTEGER PRIMARY KEY AUTO_INCREMENT, name varchar(255) NOT NULL UNIQUE, age INTEGER NOT NULL, created_at datetime DEFAULT NULL DEFAULT NOW(), updated_at datetime DEFAULT NULL DEFAULT NOW() ON UPDATE NOW())")
+	handleError(err)
+
+	// just a check
+	_, err = db.Exec("SELECT COUNT(*) FROM suppliers;")
+	handleError(err)
+}
+
+func showMenu() {
+	var input string
+
+	fmt.Println("1- List suppliers")
+	fmt.Println("2- Age of supplier")
+	fmt.Print("Enter option: ")
+
+	fmt.Scanln(&input)
+	fmt.Print(input)
+}
+
+func main() {
+
+	if len(os.Args) < 2 {
+		fmt.Println("Missing XML file")
 	} else {
-		fmt.Println(processXML(os.Args[1], db))
+		configDB()
+		xmlData := readXML(os.Args[1], db)
+		insertIntoDB(xmlData)
+		DBstats()
+		showMenu()
 	}
 
 	defer db.Close()
